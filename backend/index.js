45f40971646e87b9c8e4300cfcc2f55d71a2eb53
madhanmatthew <madhan4530@gmail.com>
2025-11-13 11:42:02 +0530

    fix:updated backend/index.js fixing images problem and added path func

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { Pool } = require('pg');
 const http = require('http');
 const { Server } = require('socket.io');
